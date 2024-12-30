1338108dfaad782b4fc3cdbaf7b3ee713023c15d
Ashu-Thakur <asthakur@bestpeers.com>
2024-12-30 17:33:33 +0530

    push the final changes

--- main_thought_stream/post/serializers.py
+++ main_thought_stream/post/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Post, Comment
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username'] 
 
 class CommentSerializer(serializers.ModelSerializer):
-    # This will serialize the replies for a given comment
+    # Use the UserSerializer to represent the user field with both id and username
+    user = UserSerializer(read_only=True)
     replies = serializers.SerializerMethodField()
 
     class Meta:
@@ -18,27 +25,30 @@ class CommentSerializer(serializers.ModelSerializer):
         comment = Comment.objects.create(**validated_data)
         return comment
 
-
 class PostSerializer(serializers.ModelSerializer):
+    # Use the UserSerializer to represent the author field with both id and username
+    author = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'author', 'content', 'image', 'created_at', 'updated_at', 'comments']
-        read_only_fields = ['created_at', 'updated_at', 'author']  
+        read_only_fields = ['created_at', 'updated_at', 'author']
+
     def create(self, validated_data):
         user = self.context['request'].user
-        validated_data['author'] = user  
+        validated_data['author'] = user
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
         validated_data.pop('author', None)
-        
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        
+
         instance.save()
         return instance
+
 
 
         
